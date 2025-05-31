daee4cb1ae03385b165c0a075e520569ebb9575f
zz1000-god <maksmine353@gmail.com>
2025-05-31 08:57:10 +0300

    Update hud_timer.cpp

--- cl_dll/hud_timer.cpp
+++ cl_dll/hud_timer.cpp
@@ -1,101 +1,288 @@
+#include <time.h>
 #include "hud.h"
 #include "cl_util.h"
 #include "parsemsg.h"
-#include "discord_integration.h"
+#include "net_api.h"
+#include "demo_api.h"
 
-DECLARE_MESSAGE(m_Timer, Timer);
+#define TIMER_Y             0.04f
+#define TIMER_Y_NEXT_OFFSET 0.04f
+#define TIMER_RED_R         255
+#define TIMER_RED_G         16
+#define TIMER_RED_B         16
+#define CUSTOM_TIMER_R      0
+#define CUSTOM_TIMER_G      160
+#define CUSTOM_TIMER_B      0
+#define MAX_CUSTOM_TIMERS   4
 
-static void unpack_seconds(int seconds_total, int& days, int& hours, int& minutes, int& seconds)
+CON_COMMAND(customtimer, "Sets a timer to count down from N secs to zero")
 {
-	constexpr int SECONDS_PER_MINUTE = 60;
-	constexpr int SECONDS_PER_HOUR = SECONDS_PER_MINUTE * 60;
-	constexpr int SECONDS_PER_DAY = SECONDS_PER_HOUR * 24;
-
-	days = seconds_total / SECONDS_PER_DAY;
-	seconds_total %= SECONDS_PER_DAY;
-
-	hours = seconds_total / SECONDS_PER_HOUR;
-	seconds_total %= SECONDS_PER_HOUR;
-
-	minutes = seconds_total / SECONDS_PER_MINUTE;
-	seconds_total %= SECONDS_PER_MINUTE;
-
-	seconds = seconds_total;
+    CHudTimer::Get()->CustomTimerCommand();
 }
 
-int CHudTimer::Init()
+class CHudTimer : public BaseHudClass
 {
-	HOOK_MESSAGE(Timer);
+public:
+    static CHudTimer* Get()
+    {
+        static CHudTimer s_Timer;
+        return &s_Timer;
+    }
 
-	m_iFlags = 0;
-	hud_timer = CVAR_CREATE("hud_timer", "1", FCVAR_ARCHIVE);
+    void Init()
+    {
+        BaseHudClass::Init();
 
-	gHUD.AddHudElem(this);
-	return 0;
-}
+        HookMessage<&CHudTimer::MsgFunc_Timer>("Timer");
 
-int CHudTimer::VidInit()
-{
-	m_iFlags &= ~HUD_ACTIVE;
+        m_iFlags |= HUD_ACTIVE;
 
-	return 1;
-}
+        m_pCvarHudTimer = gEngfuncs.pfnRegisterVariable("hud_timer", "1", FCVAR_BHL_ARCHIVE);
+        m_pCvarHudTimerSync = gEngfuncs.pfnRegisterVariable("hud_timer_sync", "1", FCVAR_BHL_ARCHIVE);
+        m_pCvarHudNextmap = gEngfuncs.pfnRegisterVariable("hud_nextmap", "1", FCVAR_BHL_ARCHIVE);
 
-int CHudTimer::Draw(float time)
-{
-	if (gHUD.m_flTime >= draw_until) {
-		m_iFlags &= ~HUD_ACTIVE;
-		return 0;
-	}
+        memset(m_flCustomTimerStart, 0, sizeof(m_flCustomTimerStart));
+        memset(m_flCustomTimerEnd, 0, sizeof(m_flCustomTimerEnd));
+        memset(m_bCustomTimerNeedSound, 0, sizeof(m_bCustomTimerNeedSound));
+        m_szNextmap[0] = 0;
+    }
 
-	if (hud_timer->value == 0.0f)
-		return 0;
+    void VidInit()
+    {
+        m_iFlags |= HUD_ACTIVE;
+        m_flEndTime = 0;
+        m_flEffectiveTime = 0;
+        m_flSynced = false;
+        m_flDemoSyncTime = 0;
+        m_bDemoSyncTimeValid = false;
+        memset(m_flCustomTimerStart, 0, sizeof(m_flCustomTimerStart));
+        memset(m_flCustomTimerEnd, 0, sizeof(m_flCustomTimerEnd));
+        memset(m_bCustomTimerNeedSound, 0, sizeof(m_bCustomTimerNeedSound));
+        m_szNextmap[0] = 0;
+    }
 
-	char str[32];
+    int MsgFunc_Timer(const char* pszName, int iSize, void* pbuf)
+    {
+        BEGIN_READ(pbuf, iSize);
+        m_flEndTime = (float)READ_LONG();
+        m_flEffectiveTime = (float)READ_LONG();
+        m_flSynced = true;
+        m_iFlags |= HUD_ACTIVE;
+        return 1;
+    }
 
-	int seconds_to_draw;
-	if (hud_timer->value == 2.0f || seconds_total == 0)
-		seconds_to_draw = seconds_passed;
-	else
-		seconds_to_draw = seconds_total - seconds_passed;
+    void Draw(float fTime)
+    {
+        char text[128];
 
-	int days, hours, minutes, seconds;
-	unpack_seconds(seconds_to_draw, days, hours, minutes, seconds);
+        if (gHUD.m_iHideHUDDisplay & HIDEHUD_ALL)
+            return;
 
-	if (days > 0)
-		sprintf(str, "%d day%s %dh %dm %ds", days, (days > 1 ? "s" : ""), hours, minutes, seconds);
-	else if (hours > 0)
-		sprintf(str, "%dh %dm %ds", hours, minutes, seconds);
-	else if (minutes > 0)
-		sprintf(str, "%d:%02d", minutes, seconds);
-	else if (seconds_to_draw >= 0)
-		sprintf(str, "%d", seconds);
-	else
-		// Overtime!
-		sprintf(str, "%d", seconds_to_draw);
+        // Time source: demo or real time
+        float currentTime;
+        if (gEngfuncs.pDemoAPI->IsPlayingback())
+        {
+            if (m_bDemoSyncTimeValid)
+                currentTime = m_flDemoSyncTime + fTime;
+            else
+                currentTime = 0;
+        }
+        else
+        {
+            currentTime = fTime;
+        }
 
-	int r, g, b;
-	UnpackRGB(r, g, b, gHUD.m_iDefaultHUDColor);
+        // HUD color
+        int r, g, b;
+        float a = 255 * gHUD.GetHudTransparency();
+        gHUD.GetHudColor(HudPart::Common, 0, r, g, b);
+        ScaleColors(r, g, b, a);
 
-	gHUD.DrawHudStringCentered(ScreenWidth / 2, gHUD.m_scrinfo.iCharHeight, str, r, g, b);
+        // Main timer
+        float timeleft = m_flSynced ? (int)(m_flEndTime - currentTime) + 1 : (int)(m_flEndTime - m_flEffectiveTime);
+        int hud_timer = (int)m_pCvarHudTimer->value;
+        int ypos = ScreenHeight * TIMER_Y;
+        switch (hud_timer)
+        {
+        case 1: // time left
+            if (currentTime > 0 && timeleft > 0)
+                DrawTimerInternal((int)timeleft, ypos, r, g, b, true);
+            break;
+        case 2: // time passed
+            if (currentTime > 0)
+                DrawTimerInternal((int)currentTime, ypos, r, g, b, false);
+            break;
+        case 3: // local PC time
+        {
+            time_t rawtime;
+            struct tm* timeinfo;
+            time(&rawtime);
+            timeinfo = localtime(&rawtime);
+            sprintf(text, "Clock %d:%02d:%02d", (int)timeinfo->tm_hour, (int)timeinfo->tm_min, (int)timeinfo->tm_sec);
+            int width = TextMessageDrawString(ScreenWidth + 1, ypos, text, 0, 0, 0);
+            TextMessageDrawString((ScreenWidth - width) / 2, ypos, text, r, g, b);
+            break;
+        }
+        }
 
-	return 0;
-}
+        // Nextmap
+        int hud_nextmap = (int)m_pCvarHudNextmap->value;
+        if (m_szNextmap[0] && timeleft < 60 && timeleft >= 0 && m_flEndTime > 0 && (hud_nextmap == 2 || (hud_nextmap == 1 && timeleft >= 37)))
+        {
+            snprintf(text, sizeof(text), "Nextmap is %s", m_szNextmap);
+            ypos = ScreenHeight * (TIMER_Y + TIMER_Y_NEXT_OFFSET);
+            int width = TextMessageDrawString(ScreenWidth + 1, ypos, text, 0, 0, 0);
+            float a = (timeleft >= 40 || hud_nextmap > 1 ? 255.0f : 255.0f / 3 * ((m_flEndTime - currentTime) + 1 - 37)) * gHUD.GetHudTransparency();
+            gHUD.GetHudColor(HudPart::Common, 0, r, g, b);
+            ScaleColors(r, g, b, a);
+            TextMessageDrawString((ScreenWidth - width) / 2, ypos, text, r, g, b);
+        }
 
-int CHudTimer::MsgFunc_Timer(const char* name, int size, void* buf)
-{
-	BEGIN_READ(buf, size);
+        // Custom timers
+        for (int i = 0; i < MAX_CUSTOM_TIMERS; i++)
+        {
+            if (m_flCustomTimerStart[i] - currentTime > 0.5 || currentTime == 0)
+            {
+                m_flCustomTimerStart[i] = 0;
+                m_flCustomTimerEnd[i] = 0;
+                m_bCustomTimerNeedSound[i] = false;
+            }
+            else if (m_flCustomTimerEnd[i] > currentTime)
+            {
+                timeleft = (int)(m_flCustomTimerEnd[i] - currentTime) + 1;
+                sprintf(text, "Timer %d", (int)timeleft);
+                ypos = ScreenHeight * (TIMER_Y + TIMER_Y_NEXT_OFFSET * (i + 2));
+                int width = TextMessageDrawString(ScreenWidth + 1, ypos, text, 0, 0, 0);
+                float a = 255 * gHUD.GetHudTransparency();
+                r = CUSTOM_TIMER_R, g = CUSTOM_TIMER_G, b = CUSTOM_TIMER_B;
+                ScaleColors(r, g, b, a);
+                TextMessageDrawString((ScreenWidth - width) / 2, ypos, text, r, g, b);
+            }
+            else if (m_bCustomTimerNeedSound[i])
+            {
+                if (currentTime - m_flCustomTimerEnd[i] < 1.5)
+                    PlaySound("fvox/bell.wav", 1);
+                m_flCustomTimerStart[i] = 0;
+                m_flCustomTimerEnd[i] = 0;
+                m_bCustomTimerNeedSound[i] = false;
+            }
+        }
+    }
 
-	seconds_total = READ_LONG();
-	seconds_passed = READ_LONG();
+    void DrawTimerInternal(int time, float ypos, int r, int g, int b, bool redOnLow)
+    {
+        div_t q;
+        char text[64];
 
-	draw_until = gHUD.m_flTime + 5.0f;
+        if (time >= 86400)
+        {
+            q = div(time, 86400);
+            int d = q.quot;
+            q = div(q.rem, 3600);
+            int h = q.quot;
+            q = div(q.rem, 60);
+            int m = q.quot;
+            int s = q.rem;
+            sprintf(text, "%dd %dh %02dm %02ds", d, h, m, s);
+        }
+        else if (time >= 3600)
+        {
+            q = div(time, 3600);
+            int h = q.quot;
+            q = div(q.rem, 60);
+            int m = q.quot;
+            int s = q.rem;
+            sprintf(text, "%dh %02dm %02ds", h, m, s);
+        }
+        else if (time >= 60)
+        {
+            q = div(time, 60);
+            int m = q.quot;
+            int s = q.rem;
+            sprintf(text, "%d:%02d", m, s);
+        }
+        else
+        {
+            sprintf(text, "%d", (int)time);
+            if (redOnLow)
+            {
+                float a = 255 * gHUD.GetHudTransparency();
+                r = TIMER_RED_R, g = TIMER_RED_G, b = TIMER_RED_B;
+                ScaleColors(r, g, b, a);
+            }
+        }
 
-	m_iFlags |= HUD_ACTIVE;
+        int width = TextMessageDrawString(ScreenWidth + 1, ypos, text, 0, 0, 0);
+        TextMessageDrawString((ScreenWidth - width) / 2, ypos, text, r, g, b);
+    }
 
-	discord_integration::set_time_data(seconds_total, seconds_passed);
+    void CustomTimerCommand(void)
+    {
+        if (gEngfuncs.pDemoAPI->IsPlayingback())
+            return;
 
-	//gEngfuncs.Con_Printf("Timer: seconds_total = %d; seconds_passed = %d.\n", seconds_total, seconds_passed);
+        if (gEngfuncs.Cmd_Argc() <= 1)
+        {
+            gEngfuncs.Con_Printf("usage:  customtimer <interval in seconds> [timer number 1|2]\n");
+            return;
+        }
 
-	return 1;
-}
+        // Get interval value
+        int interval;
+        char* intervalString = gEngfuncs.Cmd_Argv(1);
+        if (!intervalString || !intervalString[0])
+            return;
+        interval = atoi(intervalString);
+        if (interval < 0)
+            return;
+        if (interval > 86400)
+            interval = 86400;
+
+        // Get timer number
+        int number = 0;
+        char* numberString = gEngfuncs.Cmd_Argv(2);
+        if (numberString && numberString[0])
+        {
+            number = atoi(numberString) - 1;
+            if (number < 0 || number >= MAX_CUSTOM_TIMERS)
+                return;
+        }
+
+        // Set custom timer
+        m_flCustomTimerStart[number] = gEngfuncs.GetClientTime();
+        m_flCustomTimerEnd[number] = gEngfuncs.GetClientTime() + interval;
+        m_bCustomTimerNeedSound[number] = true;
+    }
+
+    // Nextmap support
+    void SetNextmap(const char* nextmap)
+    {
+        strncpy(m_szNextmap, nextmap, sizeof(m_szNextmap) - 1);
+        m_szNextmap[sizeof(m_szNextmap) - 1] = 0;
+    }
+
+    const char* GetNextmap() const
+    {
+        return m_szNextmap;
+    }
+
+private:
+    float m_flEndTime = 0;
+    float m_flEffectiveTime = 0;
+    bool m_flSynced = false;
+    float m_flDemoSyncTime = 0;
+    bool m_bDemoSyncTimeValid = false;
+
+    cvar_t* m_pCvarHudTimer = nullptr;
+    cvar_t* m_pCvarHudTimerSync = nullptr;
+    cvar_t* m_pCvarHudNextmap = nullptr;
+
+    char m_szNextmap[64]{};
+
+    float m_flCustomTimerStart[MAX_CUSTOM_TIMERS]{};
+    float m_flCustomTimerEnd[MAX_CUSTOM_TIMERS]{};
+    bool m_bCustomTimerNeedSound[MAX_CUSTOM_TIMERS]{};
+};
+
+// HUD registration
+DEFINE_HUD_ELEM(CHudTimer);
