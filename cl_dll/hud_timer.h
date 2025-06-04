31e9d52d4b35a51f4627e914fef387f76b2d7c6e
zz1000-god <maksmine353@gmail.com>
2025-06-04 15:12:00 +0300

    Update hud_timer.h

--- cl_dll/hud_timer.h
+++ cl_dll/hud_timer.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "net.h" // Додайте цей рядок для NetSocket
 
 class CHudTimer : public CHudBase
 {
@@ -9,11 +10,12 @@ public:
 
 	int MsgFunc_Timer(const char* name, int size, void* buf);
 	void Think();
-	void SyncTimerLocal(float fTime);
+	// void SyncTimerLocal(float fTime); // Можна залишити, якщо є окрема реалізація, або інтегрувати в SyncTimer
 
 private:
 	void SyncTimer(float fTime);
 	void DoResync();
+	void SyncTimerRemote(unsigned int ip, unsigned short port, float fTime, double latency); // Оголошення нової функції
 
 	// Message data
 	int seconds_total;
@@ -23,7 +25,7 @@ private:
 	
 	// Sync data
 	float m_flEndTime;
-	float m_flEffectiveTime;
+	float m_flEffectiveTime; // Використовується, якщо m_flSynced == false
 	float m_flNextSyncTime;
 	bool m_flSynced;
 	bool m_bDelayTimeleftReading;
@@ -33,4 +35,11 @@ private:
 	cvar_t* m_pCvarHudTimerSync;
 	cvar_t* m_pCvarMpTimelimit;
 	cvar_t* m_pCvarMpTimeleft;
+
+	// Нові змінні для A2S_RULES синхронізації
+	char m_szPacketBuffer[2048]; // Буфер для збереження відповіді (можливо, фрагментованої)
+	int m_iReceivedSize;         // Загальний розмір отриманої відповіді
+	int m_iResponceID;           // ID поточної відповіді (для фрагментованих пакетів)
+	int m_iReceivedPackets;      // Бітова маска отриманих фрагментованих пакетів
+	int m_iReceivedPacketsCount; // Кількість отриманих фрагментованих пакетів
 };
