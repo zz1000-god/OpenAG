de000a43fd1944d6bc67fd1024980a1746498637
zz1000-god <maksmine353@gmail.com>
2025-06-04 12:19:48 +0300

    Update hud_timer.h

--- cl_dll/hud_timer.h
+++ cl_dll/hud_timer.h
@@ -9,10 +9,27 @@ public:
 	int VidInit();
 	int Draw(float flTime);
 	int MsgFunc_Timer(const char* name, int size, void* buf);
+	void Think();
 
 private:
+	void SyncTimer(float fTime);
+	void DoResync();
+
+	// Message data
 	int seconds_total;
 	int seconds_passed;
 	float draw_until;
+	
+	// Sync data
+	float m_flEndTime;
+	float m_flEffectiveTime;
+	float m_flNextSyncTime;
+	bool m_flSynced;
+	bool m_bDelayTimeleftReading;
+	
+	// CVars
 	cvar_t* hud_timer;
+	cvar_t* m_pCvarHudTimerSync;
+	cvar_t* m_pCvarMpTimelimit;
+	cvar_t* m_pCvarMpTimeleft;
 };
