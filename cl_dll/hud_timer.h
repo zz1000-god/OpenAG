69b753bbd9a5b83b2a114464e96c63eb52a46fce
zz1000-god <maksmine353@gmail.com>
2025-06-04 12:15:55 +0300

    Update hud_timer.h

--- cl_dll/hud_timer.h
+++ cl_dll/hud_timer.h
@@ -1,17 +1,18 @@
 #pragma once
 
+#include "hud.h"
+
 class CHudTimer : public CHudBase
 {
-	float draw_until;
+public:
+	int Init();
+	int VidInit();
+	int Draw(float flTime);
+	int MsgFunc_Timer(const char* name, int size, void* buf);
+
+private:
 	int seconds_total;
 	int seconds_passed;
-
+	float draw_until;
 	cvar_t* hud_timer;
-
-public:
-	virtual int Init();
-	virtual int VidInit();
-	virtual int Draw(float time);
-
-	int MsgFunc_Timer(const char* name, int size, void* buf);
 };
