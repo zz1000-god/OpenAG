9628517e8c1584ea00800dd4dfa1c3f143a496b4
zz1000-god <maksmine353@gmail.com>
2025-06-12 15:45:32 +0300

    Update steam_id.h

--- cl_dll/steam_id.h
+++ cl_dll/steam_id.h
@@ -9,6 +9,5 @@ namespace steam_id
 	const std::string& get_steam_id(size_t player_index);
 	bool is_showing_real_names();
 	const std::string& get_real_name(size_t player_index);
-	CPlayerInfo *GetPlayerInfoSafe(int idx);
 
 }
